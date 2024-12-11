b6b6ceb36a92e0e1e8fd211e4ebbda8fec65d87e
Sinan Suludağ <sinan.sldg23@gmail.com>
2024-12-11 11:47:39 +0300

    ColorScheme içerisindeki renkler ortak bir renk sınıfında tanimli renklerden alindi.

---
+++ lib/core/constants/colors/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Ortak renkler
+  static const Color primaryDark = Color(0xFF212121);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color yellow = Color(0xFFFFC107);
+  static const Color black = Color(0xFF000000);
+  static const Color lightGrey = Color(0xFFF5F5F5);
+  static const Color darkGreen = Color(0xFF81C784);
+  static const Color paleYellow = Color(0xFFFFE082);
+  static const Color darkGrey = Color(0xFF424242);
+  static const Color red = Color(0xFFD32F2F);
+  static const Color lightRed = Color(0xFFE57373);
+}
--- lib/core/theme/color_scheme.dart
+++ lib/core/theme/color_scheme.dart
@@ -1,27 +1,28 @@
+import 'package:asset_tracker/core/constants/colors/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AppColorScheme {
   static const lightColorScheme = ColorScheme(
     brightness: Brightness.light,
-    primary: Color(0xFF212121),
-    onPrimary: Color(0xFFFFFFFF),
-    secondary: Color(0xFFFFC107),
-    onSecondary: Color(0xFF000000),
-    surface: Color(0xFFF5F5F5),
-    onSurface: Color(0xFF212121),
-    error: Color(0xFFD32F2F),
-    onError: Color(0xFFFFFFFF),
+    primary: AppColors.primaryDark,
+    onPrimary: AppColors.white,
+    secondary: AppColors.yellow,
+    onSecondary: AppColors.black,
+    surface: AppColors.lightGrey,
+    onSurface: AppColors.primaryDark,
+    error: AppColors.red,
+    onError: AppColors.white,
   );
 
   static const darkColorScheme = ColorScheme(
     brightness: Brightness.dark,
-    primary: Color(0xFF81C784),
-    onPrimary: Color(0xFF000000),
-    secondary: Color(0xFFFFE082),
-    onSecondary: Color(0xFF000000),
-    surface: Color(0xFF424242),
-    onSurface: Color(0xFFF5F5F5),
-    error: Color(0xFFE57373),
-    onError: Color(0xFF000000),
+    primary: AppColors.darkGreen,
+    onPrimary: AppColors.black,
+    secondary: AppColors.paleYellow,
+    onSecondary: AppColors.black,
+    surface: AppColors.darkGrey,
+    onSurface: AppColors.lightGrey,
+    error: AppColors.lightRed,
+    onError: AppColors.black,
   );
 }
