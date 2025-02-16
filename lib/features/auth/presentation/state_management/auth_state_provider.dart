0842ed59c2cc2deda87e91729b205c769d9897f1
Sinan Suludağ <sinan.sldg23@gmail.com>
2025-02-16 22:57:47 +0300

    firestore islemleri icin katmanlı mimari islemi yapildi.

--- lib/features/auth/presentation/state_management/auth_state_provider.dart
+++ lib/features/auth/presentation/state_management/auth_state_provider.dart
@@ -2,11 +2,9 @@ import 'package:asset_tracker/features/auth/data/datasources/local/abstract_loca
 import 'package:asset_tracker/features/auth/data/datasources/local/local_storage_service.dart';
 import 'package:asset_tracker/features/auth/data/datasources/remote/abstract_auth_service.dart';
 import 'package:asset_tracker/features/auth/data/datasources/remote/firebase_auth_service.dart';
-import 'package:asset_tracker/features/auth/data/datasources/remote/mock_auth_service.dart';
 import 'package:asset_tracker/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:asset_tracker/features/auth/presentation/state_management/auth_state_manager.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
@@ -22,7 +20,7 @@ final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
 
 //Firebase ve mock servislerini injekte eden yer
 final authServiceProvider = Provider<IAuthService>((ref) {
-  return MockAuthServiceImpl();
+  return FirebaseAuthServiceImpl(FirebaseAuth.instance); // Firebase servisi
 });
 
 final localStorageServiceProvider = Provider<ILocalStorageService>((ref) {
